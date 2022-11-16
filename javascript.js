ce27ce0435f1543f391eb8a4a77418e06e788c76
axolotlfish <canlascyrilchristian@gmail.com>
2022-11-16 13:04:45 +0800

    added 3 Buttons instead of typing what to choose between rock paper and scissors

--- javascript.js
+++ javascript.js
@@ -1,6 +1,38 @@
 let playerScore = 0;
 let AIScore = 0;
 
+const button1 = document.querySelector('#rock');
+const button2 = document.querySelector('#paper');
+const button3 = document.querySelector('#scissors');
+
+const header2 = document.querySelector('.header2');
+header2.setAttribute('style','color:crimson','background: black');
+header2.textContent = "Choose Your Weapon!";
+
+const winOrLose = document.querySelector('#winOrLose');
+const choose = document.querySelector('.choose');
+const score = document.querySelector('.score');
+const result = document.querySelector('.result');
+
+
+
+
+
+button1.addEventListener('click',() => {
+    playRound("rock");
+    
+    
+});
+button2.addEventListener('click',() => {
+    playRound("paper");
+    
+});
+button3.addEventListener('click',() => {
+    playRound("scissor");
+});
+
+
+
 
 function playRound(playerSelection,computerSelection){
 
@@ -22,23 +54,26 @@ function playRound(playerSelection,computerSelection){
     }
     
 
-    let playerSelectionContainer = prompt("choose your weapon: Rock, Scissors or Paper");
+   /* let playerSelectionContainer = prompt("choose your weapon: Rock, Scissors or Paper");
     let playerSelectionContainerSmall = playerSelectionContainer.toLowerCase();
-    playerSelection = playerSelectionContainerSmall;
+    playerSelection = playerSelectionContainerSmall;*/
     
     if(playerSelection === computerSelection){
         console.log("Tie!");
+        winOrLose.textContent = "It's a Tie! You have the same weapon";
         
     }
 
     else if(playerSelection==="rock"){
         if(computerSelection==="scissor"){
             console.log("Rock beats Scissor! You Win!");
+            winOrLose.textContent = "Rock beats Scissor! You Win!";
             playerScore++;
             
         }
         else{
             console.log("Paper Beats Rock! You Lose!");
+            winOrLose.textContent = "Paper Beats Rock! You Lose!";
             AIScore++;
             
         }
@@ -46,11 +81,13 @@ function playRound(playerSelection,computerSelection){
     else if(playerSelection==="scissor"){
         if(computerSelection==="paper"){
             console.log("Scissor Beats Paper! You Win!");
+            winOrLose.textContent = "Scissor Beats Paper! You Win!";
             playerScore++;
             
         }
         else{
             console.log("Rock beats Paper! You Lose");
+            winOrLose.textContent = "Rock beats Paper! You Lose!";
             AIScore++;
             
         }
@@ -58,11 +95,13 @@ function playRound(playerSelection,computerSelection){
     else if(playerSelection==="paper"){
         if(computerSelection==="rock"){
             console.log("Paper beats Rock! You Win");
+            winOrLose.textContent = "Paper beats Rock! You Win!";
             playerScore++;
     
         }
         else{
             console.log("Scissor beats Paper! You Lose!");
+            winOrLose.textContent = "Scissor beats Paper! You Lose!";
             AIScore++;
 
 
@@ -70,14 +109,50 @@ function playRound(playerSelection,computerSelection){
     }
     else{
         console.log("not in the selection");
+        winOrLose.textContent="are you cheating?";
         
     }
     console.log("\n \n");
-    console.log("You chose:" +playerSelectionContainer);
+    console.log("You chose:" +playerSelection);
     console.log("AI chose:" +computerSelection);
     console.log("\n \n");
 
-    return playRound(playerSelection,computerSelection);
+    choose.textContent = `You chose: ${playerSelection} Your enemy chose: ${computerSelection}`;
+
+    /*
+    console.log("AIScore: " +AIScore)
+    console.log("PLayerScore: "+playerScore)*/
+    score.textContent ="You : "+playerScore +" Enemy: "+AIScore;
+    winOrLose.appendChild(choose);
+    winOrLose.appendChild(score);
+
+
+
+    if(playerScore === 5){
+        console.log("Game Over: CONGRATULATIONS! YOU WIN");
+        result.textContent="Game Over: CONGRATULATIONS! YOU WIN";
+        result.setAttribute('style','color:gold; background:grey;');
+        winOrLose.appendChild(result);
+        playerScore = 0;
+        AIScore = 0;
+
+    }
+    else if(AIScore ===5){
+        console.log("Game Over: You Lose!");
+        result.textContent="Game Over: You Lose!";
+        result.setAttribute('style', 'color: blue; background: black;');
+        winOrLose.appendChild(result);
+        playerScore = 0;
+        AIScore = 0;
+    }
+    /*else if(){
+        console.log("Game Over: TIE");
+        result.textContent="Game Over: TIE";
+    }*/
+
+    
+
+    //return playRound(playerSelection,computerSelection);
 
 
     
@@ -86,22 +161,7 @@ function playRound(playerSelection,computerSelection){
 
 function game(){
 
-for(let i = 0; i < 5; i++){
-    console.log("\n \n "+`Round: ${i}`+"\n \n")
-    playRound();
-    
-    console.log("Player's Score: "+ playerScore);
-    console.log("AI's Score: "+ AIScore);
-    }
-    if(playerScore > AIScore){
-        console.log("Game Over: CONGRATULATIONS! YOU WIN")
-    }
-    else if(AIScore > playerScore){
-        console.log("Game Over: You Lose!")
-    }
-    else{
-        console.log("Game Over: TIE")
-    }
 }
+
 
 
